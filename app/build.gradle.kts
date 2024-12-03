0f8becaacd6fd1b9ab9b38cd26b965e0c3cb4f7e
fgasparc <franciscogasparcasadogarcia@gmail.com>
2024-12-03 19:16:12 +0100

    Anim des car

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.14.2")
 }
--- app/src/main/java/com/example/crudito/MemoryTron.kt
+++ app/src/main/java/com/example/crudito/MemoryTron.kt
@@ -1,24 +1,31 @@
 package com.example.crudito
 
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.media.Image
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
+import android.view.View
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.drawable.toDrawable
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.drawToBitmap
+import com.bumptech.glide.Glide
 import com.example.crudito.databinding.ActivityMemoryTronBinding
 
 
 
 class MemoryTron : AppCompatActivity() {
     private lateinit var binding: ActivityMemoryTronBinding
-    var contador=0
-    var cartasEncontradas=mutableListOf<ImageButton?>()
-    var cartaSeleccionadas= arrayOfNulls<ImageButton>(2)
+    var contador = 0
+    var vidas = 5
+    var cartasEncontradas = mutableListOf<ImageButton?>()
+    var imagenesEncontradas = arrayOfNulls<Int>(2)
+    var cartaSeleccionadas = arrayOfNulls<ImageButton>(2)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMemoryTronBinding.inflate(layoutInflater)
@@ -55,52 +62,68 @@ class MemoryTron : AppCompatActivity() {
             R.drawable.hkespadas
         )
         barajaReversa.shuffle()
-        for(i in 0 until baraja.size){
-            voltearCarta(baraja,barajaReversa,i)
+        for (i in 0 until baraja.size) {
+            voltearCarta(baraja, barajaReversa, i)
         }
 
 
-
     }
-    fun voltearCarta(a: MutableList<ImageButton>,b: MutableList<Int>,i: Int){
-            a[i].setOnClickListener {
-                a[i].setImageResource(b[i])
-                contador++
-                a[i].isClickable=false
-                Log.d("Contador if", contador.toString())
-                cartaSeleccionadas[contador-1]=a[i]
-                Log.d("Carta selecionada", cartaSeleccionadas[contador-1].toString())
-                Log.d("Carta selecionada drawable",cartaSeleccionadas[contador-1]?.imageAlpha.toString())
-                if(contador==2){
-                    if(cartaSeleccionadas[0]==cartaSeleccionadas[1]){
-                        cartasEncontradas.add(cartaSeleccionadas[0])
-                        Log.d("Carta encontrada", cartasEncontradas[0].toString())
-                        cartasEncontradas.add(cartaSeleccionadas[1])
-                        Log.d("Carta encontrada", cartasEncontradas[1].toString())
-                    }
-                    for(i in 0 until a.size){
-                        a[i].isClickable=false
-                    }
 
-                    desvoltearCarta(a)
+    fun voltearCarta(a: MutableList<ImageButton>, b: MutableList<Int>, i: Int) {
+        a[i].setOnClickListener {
+            a[i].setImageResource(b[i])
+            contador++
+            a[i].isClickable = false
+            Log.d("Contador if", contador.toString())
+            cartaSeleccionadas[contador - 1] = a[i]
+            imagenesEncontradas[contador - 1] = b[i]
+            Log.d("Imagen seleccionada", imagenesEncontradas[contador - 1].toString())
+            Log.d("Carta selecionada", cartaSeleccionadas[contador - 1].toString())
+            if (contador == 2) {
+                if (imagenesEncontradas[0] == imagenesEncontradas[1]) {
+                    cartasEncontradas.add(cartaSeleccionadas[0])
+                    Log.d("Carta encontrada", cartasEncontradas[0].toString())
+                    cartasEncontradas.add(cartaSeleccionadas[1])
+                    Log.d("Carta encontrada", cartasEncontradas[1].toString())
+                } else {
+                    vidas--
+                    Log.d("vidas", vidas.toString())
+                    if(vidas==0) derrota(a)
                 }
-            }
-    }
-    fun desvoltearCarta(a:MutableList<ImageButton>){
-        object: CountDownTimer(1000,1000){
-            override fun onTick(millisUntilFinished: Long) {
+                for (i in 0 until a.size) {
+                    a[i].isClickable = false
+                }
 
+                desvoltearCarta(a)
+            }
+        }
+    }
+    fun desvoltearCarta(a: MutableList<ImageButton>) {
+        object : CountDownTimer(1000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
             }
             override fun onFinish() {
-                for (i in 0 until a.size){
-                    if(!cartasEncontradas.contains(a[i])) {
-                        a[i].isClickable=true
+                for (i in 0 until a.size) {
+                    if (!cartasEncontradas.contains(a[i])) {
+                        a[i].isClickable = true
                         a[i].setImageResource(R.drawable.reversocartaa)
                     }
                 }
-                contador=0
+                contador = 0
             }
         }.start()
-
+    }
+    fun derrota(a: MutableList<ImageButton>) {
+        for(i in a){
+            i.isClickable=false
+            val fadeAnimation = ObjectAnimator.ofFloat(i, "alpha", 1f, 0f)
+            fadeAnimation.duration = 1000 // Duración de la animación en milisegundos
+            fadeAnimation.startDelay = 500 // Retraso antes de que comience la animación
+            fadeAnimation.start()
+        }
+        binding.derrotaimg.visibility= View.GONE
+            Glide.with(this)
+                .load(R.drawable.hksad)
+                .into(binding.derrotaimg)
     }
 }
